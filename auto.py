5c55073b9f9e0cbbdfd157df094e9175417befd7
Prashant8282 <165475810+Prashant8282@users.noreply.github.com>
2024-04-04 07:58:59 +0530

    Update auto.py

--- auto.py
+++ auto.py
@@ -5,26 +5,25 @@ import asyncio
 TELEGRAM_BOT_TOKEN = '6459647682:AAGX4P4aozvOj9zBwzYNO3_-DDBeT_ejeIY' 
 
 # Time to wait before deleting messages (in seconds)
-DELETE_DELAY = 30  
+DELETE_DELAY = 30  
 
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 
 async def delete_message(chat_id, message_id):
-    await asyncio.sleep(DELETE_DELAY)
-    await bot.delete_message(chat_id=chat_id, message_id=message_id)
+    await asyncio.sleep(DELETE_DELAY)
+    await bot.delete_message(chat_id=chat_id, message_id=message_id)
 
 def handle_message(update, context):
-    message = update.message
-    # Schedule the message to be deleted
-    asyncio.create_task(delete_message(message.chat_id, message.message_id))
+    message = update.message
+    # Schedule the message to be deleted
+    asyncio.create_task(delete_message(message.chat_id, message.message_id))
 
-if __name__ == '__main__':
-    updater = telegram.Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+if name == 'main':
+    updater = telegram.Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
 
-    message_handler = telegram.MessageHandler(telegram.Filters.text, handle_message)
-    dispatcher.add_handler(message_handler)
+    message_handler = telegram.MessageHandler(telegram.Filters.text, handle_message)
+    dispatcher.add_handler(message_handler)
 
-    updater.start_polling()
-    updater.idle()
-    
+    updater.start_polling()
+    updater.idle()
